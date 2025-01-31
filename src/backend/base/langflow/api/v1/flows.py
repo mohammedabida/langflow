f4f3528a7712270192dfccdb6eef9b806d594b4c
Ahmed-Shilii <ahmed.shili@avaxia-group.com>
2025-01-31 05:17:20 +0100

    "update_flow_to_active" function update

--- src/backend/base/langflow/api/v1/flows.py
+++ src/backend/base/langflow/api/v1/flows.py
@@ -322,7 +322,7 @@ async def update_flow_to_active(
     flow_id: UUID,
     current_user: CurrentActiveUser,
 ):
-    """Update a flow."""
+    """Update a flow to published state."""
     settings_service = get_settings_service()
     try:
         db_flow = await _read_flow(
@@ -341,29 +341,11 @@ async def update_flow_to_active(
         db_flow.webhook = webhook_component is not None
         db_flow.updated_at = datetime.now(timezone.utc)
 
-        if db_flow.folder_id is None:
-            default_folder = (await session.exec(select(Folder).where(Folder.name == DEFAULT_FOLDER_NAME))).first()
-            if default_folder:
-                db_flow.folder_id = default_folder.id
-
         session.add(db_flow)
         await session.commit()
         await session.refresh(db_flow)
 
     except Exception as e:
-        if "UNIQUE constraint failed" in str(e):
-            # Get the name of the column that failed
-            columns = str(e).split("UNIQUE constraint failed: ")[1].split(".")[1].split("\n")[0]
-            # UNIQUE constraint failed: flow.user_id, flow.name
-            # or UNIQUE constraint failed: flow.name
-            # if the column has id in it, we want the other column
-            column = columns.split(",")[1] if "id" in columns.split(",")[0] else columns.split(",")[0]
-            raise HTTPException(
-                status_code=400, detail=f"{column.capitalize().replace('_', ' ')} must be unique"
-            ) from e
-
-        if hasattr(e, "status_code"):
-            raise HTTPException(status_code=e.status_code, detail=str(e)) from e
         raise HTTPException(status_code=500, detail=str(e)) from e
 
     return db_flow
